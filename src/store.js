44ec9d8e6a82e248a0b518907c88ffcb5e869944
Pwako11 <akolo@hotmail.com>
2021-11-11 11:21:34 -0500

    Added currentUser to reducer store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import usersReducer from './reducers/users.js';
+import currrentUser from './reducers/currentUser.js';
 // import { devToolsEnhancer } from 'redux-devtools-extension';
 
 const reducer = combineReducers({
-    users: usersReducer
+    users: usersReducer,
+    currrentUser
   })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
