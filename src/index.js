8f106a8820f36b80cdc123a72ee837071a2251a8
Pwako11 <akolo@hotmail.com>
2021-11-11 10:58:36 -0500

    tested store logic in index

--- src/index.js
+++ src/index.js
@@ -3,18 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import{Provider} from 'react-redux';
-import thunk from 'redux-thunk';
-import usersReducer from './reducers/users.js';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import store from './store.js';
 
-const reducer = combineReducers({
-  users: usersReducer
-})
-const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
-
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
