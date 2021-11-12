16c73bc65ae51d58469af61cb1d12fed7b61f356
Pwako11 <akolo@hotmail.com>
2021-11-12 00:51:34 -0500

    updated imports, added login component and Mapstateto props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+
 // import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {connect} from "react-redux"
+import Login from "./components/Login.js"
+import {getCurrentUser} from "./actions/currentUser.js"
 
 class App extends React.Component {
   
+  componentDidMount(){
+    this.props.getCurrentUser()
+
+  }
+
+
   render() {
 
     return (
       <div className="App">
         <header className="App-header">
           <h1>Welcome to Cuisine Tracker</h1>
+          <Login/>
         </header>
       </div>
     );
@@ -17,4 +28,11 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapStateToProps = state =>{
+  return{
+    currentUser: state.currentUser,
+    loggedIn: !!state.currentUser    
+  }
+}
+
+export default connect(mapStateToProps, {getCurrentUser})(App);
