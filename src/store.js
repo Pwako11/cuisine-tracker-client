0a68df41141d36e94916412a24d9cc4c946b5741
Pwako11 <akolo@hotmail.com>
2021-11-12 00:53:35 -0500

    created redux store folder

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import usersReducer from './reducers/users.js';
 import currrentUser from './reducers/currentUser.js';
+import loginForm from './reducers/loginForm.js';
 // import { devToolsEnhancer } from 'redux-devtools-extension';
 
 const reducer = combineReducers({
     users: usersReducer,
-    currrentUser
+    currrentUser, 
+    loginForm
   })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
