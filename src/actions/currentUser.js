5a67408edfb8dbfd930547cbe28ca75d6772935b
Pwako11 <akolo@hotmail.com>
2021-11-12 00:54:40 -0500

    added actions folder for all actions files

---
+++ src/actions/currentUser.js
@@ -0,0 +1,56 @@
+//synchronous action creators 
+
+export const setCurrentUser = user =>{
+    return{
+        type: "SET_CURRENT_USER",
+        user
+    }
+}
+
+
+//asynchronous action creators
+export const login = (credentials, history) =>{
+    return dispatch =>{
+        return fetch("http://localhost:3010/api/v1/login", {
+            credentials: "include", 
+            method: "POST", 
+            headers:{
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(credentials)
+        })
+        .then(response => response.json())
+        .then(response => {
+            if (response.error){
+                alert(response.error)
+            }else{
+                dispatch(setCurrentUser(response))
+            
+            }
+        })
+        .catch(console.log)
+    }
+}
+
+
+export const getCurrentUser = () =>{
+
+    return dispatch => {
+        return fetch("http://localhost:3010/api/v1/get_current_user", {
+            credentials: "include",
+            method: "GET",
+            headers:{
+                "Content-Type": "application/json"
+            },
+        })
+        .then( response => response.json())
+        .then(response =>{
+            if(response.error){
+                alert(response.error)
+            }else{
+                dispatch(setCurrentUser(response))
+            }
+        })
+        .catch(console.log)
+    }
+}
