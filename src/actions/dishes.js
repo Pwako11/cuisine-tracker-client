495b9d8a3c08c1a6433be0d67f727ad28127a46d
Pwako11 <akolo@hotmail.com>
2021-11-15 06:25:05 -0500

    created a dishes action file

---
+++ src/actions/dishes.js
@@ -0,0 +1,57 @@
+//synchronous action creators 
+
+export const setDish = dish =>{
+    return{
+        type: "SET_MY_DISH",
+        dish
+    }
+}
+
+export const addDish = dish =>{
+    return{
+        type: "ADD_DISH",
+        dish
+    }
+}
+
+export const updateDishSuccess = dish => {
+    return{
+        type: "UPDATE_DISH",
+        dish
+    }
+}
+
+export const clearDish = () =>{
+    return {
+        type: "CLEAR_CURRENT_DISH"
+    }
+}
+
+export const deleteDishSuccess = dish => {
+    return{
+        type: "DELETE_DISH",
+        dish
+    }
+}
+
+//asynchronous action creators
+export const getDishes = () =>{
+    return dispatch =>{
+        return fetch("http://localhost:3010/api/v1/dishes", {
+            credentials: "include", 
+            method: "GET", 
+            headers:{
+                "content-type": "application/json"
+            },
+        })
+        .then(response => response.json())
+        .then(response => {
+            if (response.error){
+                alert(response.error)
+            }else{
+                dispatch(setDish(response))        
+            }
+        })
+        .catch(console.log)
+    }
+}
