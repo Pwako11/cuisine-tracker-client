78202ca4401c4d08f976f0ecb2141d2283b9ce85
Pwako11 <akolo@hotmail.com>
2021-11-15 11:40:14 -0500

    Added dish to store

--- src/store.js
+++ src/store.js
@@ -3,12 +3,14 @@ import thunk from 'redux-thunk';
 import usersReducer from './reducers/users.js';
 import currentUser from './reducers/currentUser.js';
 import loginForm from './reducers/loginForm.js';
+import dish from './reducers/dish.js'
 // import { devToolsEnhancer } from 'redux-devtools-extension';
 
 const reducer = combineReducers({
     users: usersReducer,
     currentUser, 
-    loginForm
+    loginForm, 
+    dish
   })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
