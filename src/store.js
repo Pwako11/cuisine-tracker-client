d7271951a8132ca4040b74a19706a458b68f7462
Pwako11 <akolo@hotmail.com>
2021-11-17 03:08:46 -0500

    added signup form to store

--- src/store.js
+++ src/store.js
@@ -3,14 +3,16 @@ import thunk from 'redux-thunk';
 import usersReducer from './reducers/users.js';
 import currentUser from './reducers/currentUser.js';
 import loginForm from './reducers/loginForm.js';
-import dish from './reducers/dish.js'
+import dish from './reducers/dish.js';
+import signUpForm from './reducers/signupForm.js';
 // import { devToolsEnhancer } from 'redux-devtools-extension';
 
 const reducer = combineReducers({
     users: usersReducer,
     currentUser, 
     loginForm, 
-    dish
+    dish, 
+    signUpForm
   })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
