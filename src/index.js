a7c8e04087aa914a24c079ebab8c7719eb5e8245
Pwako11 <akolo@hotmail.com>
2021-11-17 03:07:53 -0500

    updated exports for react dom router

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router} from "react-router-dom"
-import{Provider} from 'react-redux';
+import React from 'react';
 import store from './store.js';
-
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+      <Router>
+        <App />
+      </Router>
   </Provider>,
   document.getElementById('root')
 );
@@ -21,3 +20,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+export default store;
