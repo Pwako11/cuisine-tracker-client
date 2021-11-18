b0220ca7def8b1a9db1bde5fca1c0db00b2b166b
Pwako11 <akolo@hotmail.com>
2021-11-17 22:57:37 -0500

    added dishes/new route

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import React from 'react';
 import { connect } from "react-redux";
 import Home from "./components/Home.js";
 import Login from "./components/Login.js";
+import Dishes from './components/Dish.js';
+import NewDishForm from './components/NewDish.js';
 import Logout from './components/Logout.js';
 import Signup from './components/Signup.js';
 import NavBar from './components/NavBar.js';
@@ -49,7 +51,9 @@ class App extends React.Component {
                 <Route exact path="/" render={() => loggedIn ? <MainContainer /> : <Home />} />
                 <Route exact path="/login" component= {Login} />
                 <Route exact path="/logout" component= {Logout} />
-                <Route exact path="/signup" component= {Signup } />   
+                <Route exact path="/signup" component= {Signup } />
+                <Route exact path="/dishes" component= {Dishes } />
+                <Route exact path="/dishes/new" component= {NewDishForm } />
             
               </Switch>
             </div> 
