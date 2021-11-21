e3db9584dc71811a882446fa8347330f357d0c33
Pwako11 <akolo@hotmail.com>
2021-11-20 23:51:02 -0500

    added searchform to the store

--- src/store.js
+++ src/store.js
@@ -6,6 +6,7 @@ import loginForm from './reducers/loginForm.js';
 import dish from './reducers/dish.js';
 import signUpForm from './reducers/signupForm.js';
 import newDishForm from './reducers/newDishForm.js';
+import dishMatchForm from './reducers/dishMatchForm.js';
 
 const reducer = combineReducers({
     users: usersReducer,
@@ -13,7 +14,8 @@ const reducer = combineReducers({
     currentUser, 
     loginForm,  
     signUpForm, 
-    newDishForm
+    newDishForm, 
+    dishMatchForm
   })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
