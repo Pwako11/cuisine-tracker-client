288326acd711a6a67a03fc941c98090f4ec996ac
Pwako11 <akolo@hotmail.com>
2021-12-21 20:09:38 -0500

    removed console.logs from file

--- src/components/Dish.js
+++ src/components/Dish.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import DishCard from './DishCard';
 import {Link} from 'react-router-dom';
 
 const Dishes = ({dishes, history}) => {
