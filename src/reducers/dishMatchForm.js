56631108883a8cb79784693e9ef0f2b65e517dbc
Pwako11 <akolo@hotmail.com>
2021-12-21 20:18:52 -0500

    removed console.logs from file

--- src/reducers/dishMatchForm.js
+++ src/reducers/dishMatchForm.js
@@ -5,7 +5,7 @@ const initialState = {
 export default (state=initialState, action) =>{
     switch(action.type){
         case "UPDATE_MATCH_FORM":
-            console.log("step 2", action.formData)
+            console.log("step 3", action.formData)
             return action.formData
         case "RESET_MATCH_FORM":
             return initialState
